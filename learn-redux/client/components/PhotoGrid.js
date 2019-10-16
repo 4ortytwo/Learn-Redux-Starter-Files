3873e42b55e5aec4f01387f6bc6940b073b2df33
aburgan <44406493+4ortytwo@users.noreply.github.com>
2019-10-16 20:55:40 +0200

    change prettier config

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const PhotoGrid = React.createClass({
   render() {
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,25 +1,27 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 
 // import CSS
-import css from "./styles/style.styl";
+import css from './styles/style.styl';
 
 // import Components
-import Main from "./components/Main";
-import Single from "./components/Single";
-import PhotoGrid from "./components/PhotoGrid";
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
 // import react router deps
 
-import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 const router = (
   <Router hitory={browserHistory}>
     <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
+      <IndexRoute component={PhotoGrid}> </IndexRoute>{' '}
+      <Route path="/view/:postId" component={Single}>
+        {' '}
+      </Route>{' '}
+    </Route>{' '}
   </Router>
 );
 
-render(router, document.getElementById("root"));
+render(router, document.getElementById('root'));
