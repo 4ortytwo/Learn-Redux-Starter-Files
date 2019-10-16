e02dfc61cf2ea08d953190d9c22981afe2cdbeb2
aburgan <44406493+4ortytwo@users.noreply.github.com>
2019-10-17 00:00:42 +0200

    connect store

---
+++ learn-redux/client/store/reducers/posts.js
@@ -0,0 +1,12 @@
+// reducer takes two things
+
+// action
+// copy of current state
+
+function posts(state = [], action) {
+  console.log('The post will change');
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
