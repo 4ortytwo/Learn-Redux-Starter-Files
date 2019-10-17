7d43ae5e6e43c108f9e57bc00c4af7c9053ca014
Arthur Burgan <arthur@ijsberen.io>
2019-10-17 18:11:44 +0200

    display state

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const Main = React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>
-          <Link to="/">Reduxstagram</Link>
-        </h1>
-        {React.cloneElement(this.props.children, this.props)}
-      </div>
-    );
-  }
-});
+const Main = () => (
+  <div>
+    <h1>
+      <Link to="/">Reduxstagram</Link>
+    </h1>
+    {React.cloneElement(this.props.children, this.props)}
+  </div>
+);
 
 export default Main;
---
+++ learn-redux/client/components/Photo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Photo = () => (
+  <figure className="grid-figure">I am a photo</figure>
+)
+
+export default Photo;
